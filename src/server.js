6b9b6ca0d729948a2f288ea2b6db5b3e4c0446d9
ricardotavaresit <ricardotavaresit@gmail.com>
2021-08-22 11:23:57 +0100

    feat: Create server file

--- src/server.js
+++ src/server.js
@@ -2,11 +2,6 @@ const app = require("./app");
 const config = require("./config/server");
 const port = config.port;
 
-
-
-
-
-
 app.listen(process.env.PORT || port, () =>
    console.log(`Running in ${port}`)
 );
