9569a4231737ba1af9d72c4648c707ef2d97be85
ricardotavaresit <ricardotavaresit@gmail.com>
2021-08-22 11:21:02 +0100

    feat: Create routes file

--- src/routes.js
+++ src/routes.js
@@ -2,16 +2,10 @@ const express = require("express");
 
 const WeatherController = require("./controllers/WeatherController");
 const LogController = require("./controllers/LogController");
-
 const routes = express.Router();
 
 routes.get("/weather/:id", WeatherController.index);
-
 routes.get("/logs", LogController.index);
 routes.post("/logs", LogController.store);
-/*
-routes.get("/teste", (req, res) => {
-   return res.json('weather');
-});
-*/
+
 module.exports = routes;
